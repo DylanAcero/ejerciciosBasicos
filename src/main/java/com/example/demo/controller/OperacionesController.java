5888f8da0729760c06b0276f75fa3eb9acd9ba42
Dylan <dylanacero318@gmail.com>
2025-01-27 21:13:43 -0600

    Limpieza de clases

--- src/main/java/com/example/demo/controller/OperacionesController.java
+++ src/main/java/com/example/demo/controller/OperacionesController.java
@@ -14,36 +14,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/operaciones")
 public class OperacionesController {
 
-    @Autowired
-    private OperacionesService operacionesService;
-
-    @GetMapping("/suma")
-    Document suma(@RequestParam(name = "num1") Double valor1,
-                  @RequestParam(name = "num2") Double valor2) {
-        return operacionesService.suma(valor1, valor2);
-    }
-
-    @GetMapping("/multiplicacion")
-    ResponseEntity<?> multiplicacion(@RequestParam(name = "num1") Double valor1,
-                                     @RequestParam(name = "num2") Double valor2){
-        return ResponseEntity.ok().body(operacionesService.multiplicacion(valor1,valor2));
-    }
-    @GetMapping("/resta")
-    ResponseEntity<?> resta(@RequestParam(name = "num1") Double valor1,
-                            @RequestParam(name = "num2") Double valor2){
-        return ResponseEntity.ok().body(operacionesService.resta(valor1,valor2));
-    }
-
-    @GetMapping("/division")
-    ResponseEntity <?> division(@RequestParam(name = "num1") Double valor1,
-                                @RequestParam(name = "num2") Double valor2) {
-        try {
-            Document res = operacionesService.division(valor1, valor2);
-
-        } catch (ArithmeticException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage()); // En caso de error con el divisor 0
-        }
-
-        return ResponseEntity.ok().body(operacionesService.division(valor1,valor2));
-    }
 }
--- src/main/java/com/example/demo/service/OperacionesService.java
+++ src/main/java/com/example/demo/service/OperacionesService.java
@@ -4,11 +4,5 @@ import org.bson.Document;
 
 public interface OperacionesService {
 
-    Document suma(Double num1, Double num2);
 
-    Document multiplicacion(Double num1, Double num2);
-
-    Document resta(Double num1, Double num2);
-
-    Document division(Double num1, Double num2);
 }
